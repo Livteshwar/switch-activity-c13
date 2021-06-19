359a76c1857af3be2cacefb441da9f418c9593e3
Livteshwar <84121496+Livteshwar@users.noreply.github.com>
2021-06-19 19:26:36 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var nums = [1,2,3,4,5];
+
+function array_square(a)
+{for (var i=0;i<a.length;i++ ){
+  a[i]=a[i]*a[i];
+
+}
+  return a
+}
+
+function setup() {
+  createCanvas(400, 400);
+  var result=array_square(nums)
+  console.log("The square is "+ result)
+ 
+}
+
+function draw() {
+  background(220);
+}
